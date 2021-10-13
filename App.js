dc082fff2e6c3919f5770bcdd11c48cc108647be
Felipe Alves <felipealves1550@gmail.com>
2021-10-13 19:31:29 -0300

    First Commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {} from 'react-native'
+import Autor from './src/Components/Autor'
+
+export default()=>{
+
+  return(
+    <Autor></Autor>
+  )
+}
---
+++ src/Components/Autor.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+export default () => {
+
+    return (
+        <View>
+             {/* <Gravatar options={{email: "anapaulanelato@gmail.com", secure: true}}></Gravatar> */}
+            <Text>Ana Paula</Text>
+        </View>
+    )
+
+}
